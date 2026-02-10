c051690cd36aed1b845cd84d98ad1c888adc0017
Linux Adona <linuxadona17@gmail.com>
2026-02-11 00:37:32 +0800

    style package included

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@ if ! command -v yay &>/dev/null; then
   rm -rf yay
 fi
 
-if [ -d "~/.tmux/plugins/tpm" ]; then
+if [ -d "$HOME/.tmux/plugins/tpm" ]; then
   echo "TPM not found. Installing now."
   git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 else
@@ -72,7 +72,8 @@ sudo pacman -S --needed --noconfirm \
   "${PACKAGES[@]}" \
   "${DEV_PACKAGES[@]}" \
   "${SHELL_PACKAGES[@]}" \
-  "${UTIL_PACKAGES[@]}"
+  "${UTIL_PACKAGES[@]}" \
+  "${STYLE_PACKAGES[@]}"
 
 yay -S --needed --noconfirm "${AUR_PACKAGES[@]}"
 
