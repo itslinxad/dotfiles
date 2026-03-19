c15467f5043c0c39ffaaa0ec144191e3051ba64d
Linux Adona <linuxadona17@gmail.com>
2026-03-19 16:22:44 +0800

    live preview plugin

--- nvim/.config/nvim/lua/plugins/live-preview.lua
+++ nvim/.config/nvim/lua/plugins/live-preview.lua
@@ -1,8 +1,12 @@
 return {
   {
     "brianhuster/live-preview.nvim",
-    config = function()
-      require("livepreview").setup()
-    end,
+    dependencies = {
+      -- You can choose one of the following pickers
+      "nvim-telescope/telescope.nvim",
+      "ibhagwan/fzf-lua",
+      "echasnovski/mini.pick",
+      "folke/snacks.nvim",
+    },
   },
 }
